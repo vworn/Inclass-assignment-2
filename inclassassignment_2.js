38e03c738e7909ad0f99f740f364e92a2a3040a4
vworn <jasonkmathewpersonal@gmail.com>
2025-02-11 14:28:31 -0500

    Task 1

--- inclassassignment_2.js
+++ inclassassignment_2.js
@@ -1,3 +1,16 @@
 // Task 1
 
+let products = [
+    { name: "Laptop", price: 1200, category: "Electronics" },
+    { name: "Phone", price: 800, category: "Electronics" },
+    { name: "Desk Chair", price: 150, category: "Furniture" },
+    { name: "Notebook", price: 5, category: "Stationery" },
+    { name: "Headphones", price: 100, category: "Electronics" }
+];
+
+function getProductsbyCategory(products, cataegory) {
+    products.filter(product => product.category === cataegory);
+}
+
+console.log(getProductsbyCategory(products, 'Electronics'));
 
